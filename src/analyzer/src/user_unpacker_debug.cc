59c7e028000036017a861c9b0d8aae03bb1723d4
SKS <sks@sksterm4.user.k18net>
2015-04-06 15:42:43 +0900

    honda

--- src/analyzer/src/user_unpacker_debug.cc
+++ src/analyzer/src/user_unpacker_debug.cc
@@ -6,6 +6,7 @@
 
 #include "user_analyzer.hh"
 #include "UnpackerManager.hh"
+#include "DAQNode.hh"
 #include "ConfMan.hh"
 
 namespace analyzer
@@ -36,6 +37,18 @@ process_event()
 {
   sleep(1);
   //  GUnpacker::get_instance().dump_data_fe(131);
+  UnpackerManager& g_unpacker = GUnpacker::get_instance();
+  int node_id = g_unpacker.get_fe_id("vme01");
+  std::cout << std::hex;
+  std::cout << g_unpacker.get_node_header(node_id, DAQNode::k_magic) << std::dec << std::endl;
+  std::cout << g_unpacker.get_node_header(node_id, DAQNode::k_data_size) << std::endl;
+  std::cout << g_unpacker.get_node_header(node_id, DAQNode::k_event_number) << std::endl;
+  std::cout << g_unpacker.get_node_header(node_id, DAQNode::k_run_number) << std::hex <<std::endl;
+  std::cout << g_unpacker.get_node_header(node_id, DAQNode::k_node_id) << std::endl;
+  std::cout << g_unpacker.get_node_header(node_id, DAQNode::k_node_type) << std::dec <<std::endl;
+  std::cout << g_unpacker.get_node_header(node_id, DAQNode::k_number_of_blocks) << std::dec <<std::endl;
+  std::cout << g_unpacker.get_node_header(node_id, DAQNode::k_unix_time) << std::dec <<std::endl;
+
   return 0;
 }
 

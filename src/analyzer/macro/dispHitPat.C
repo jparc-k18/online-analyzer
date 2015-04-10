a2b1690ac079f68f0d89972bd78b204cd838c2ff
SKS <sks@sksterm4.user.k18net>
2015-04-10 18:59:02 +0900

    modifiy dispHitPat.C

--- src/analyzer/macro/dispHitPat.C
+++ src/analyzer/macro/dispHitPat.C
@@ -14,39 +14,57 @@ void dispHitPat()
   c->Clear();
   c->Divide(4,2);
 
+  TH1* tmp = NULL;
+
   // beam
   c->cd(1);
   int base_id = HistMaker::getUniqueID(kBH1, 0, kHitPat);
-  GHist::get(base_id)->Draw();
+  tmp = (TH1*)GHist::get(base_id)->Clone();
+  tmp->SetMinimum(0);
+  tmp->Draw();
 
   c->cd(2);
   base_id = HistMaker::getUniqueID(kBFT, 0, kHitPat);
-  GHist::get(base_id)->Draw();
+  tmp = (TH1*)GHist::get(base_id)->Clone();
+  tmp->SetMinimum(0);
+  tmp->Draw();
 
   c->cd(3);
   base_id = HistMaker::getUniqueID(kBC4, 0, kHitPat, 6);
-  GHist::get(base_id)->Draw();
+  tmp = (TH1*)GHist::get(base_id)->Clone();
+  tmp->SetMinimum(0);
+  tmp->Draw();
 
   c->cd(4);
   base_id = HistMaker::getUniqueID(kBH2, 0, kHitPat);
-  GHist::get(base_id)->Draw();
+  tmp = (TH1*)GHist::get(base_id)->Clone();
+  tmp->SetMinimum(0);
+  tmp->Draw();
 
   // sks
   c->cd(5);
   base_id = HistMaker::getUniqueID(kSDC2, 0, kHitPat);
-  GHist::get(base_id)->Draw();
+  tmp = (TH1*)GHist::get(base_id)->Clone();
+  tmp->SetMinimum(0);
+  tmp->Draw();
 
   c->cd(6);
   base_id = HistMaker::getUniqueID(kSDC3, 0, kHitPat, 2);
-  GHist::get(base_id)->Draw();
+  tmp = (TH1*)GHist::get(base_id)->Clone();
+  tmp->SetMinimum(0);
+  tmp->Draw();
 
   c->cd(7);
   base_id = HistMaker::getUniqueID(kTOF, 0, kHitPat);
-  GHist::get(base_id)->Draw();
+  tmp = (TH1*)GHist::get(base_id)->Clone();
+  tmp->SetMinimum(0);
+  tmp->Draw();
 
   c->cd(8);
   base_id = HistMaker::getUniqueID(kLC, 0, kHitPat);
-  GHist::get(base_id)->Draw();
+  tmp = (TH1*)GHist::get(base_id)->Clone();
+  tmp->SetMinimum(0);
+  tmp->Draw();
 
   c->Update();
 

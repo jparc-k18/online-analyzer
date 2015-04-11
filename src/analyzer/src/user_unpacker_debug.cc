4444755090492633de848e36fc83e69f83919493
SKS <sks@sksterm4.user.k18net>
2015-04-12 04:26:38 +0900

    Add DAQ hist

--- src/analyzer/src/user_unpacker_debug.cc
+++ src/analyzer/src/user_unpacker_debug.cc
@@ -39,6 +39,7 @@ process_event()
   //  GUnpacker::get_instance().dump_data_fe(131);
   UnpackerManager& g_unpacker = GUnpacker::get_instance();
   int node_id = g_unpacker.get_fe_id("vme01");
+  //  int node_id = g_unpacker.get_fe_id("skseb"); // Event builder
   std::cout << std::hex;
   std::cout << g_unpacker.get_node_header(node_id, DAQNode::k_magic) << std::dec << std::endl;
   std::cout << g_unpacker.get_node_header(node_id, DAQNode::k_data_size) << std::endl;
@@ -49,18 +50,11 @@ process_event()
   std::cout << g_unpacker.get_node_header(node_id, DAQNode::k_number_of_blocks) << std::dec <<std::endl;
   std::cout << g_unpacker.get_node_header(node_id, DAQNode::k_unix_time) << std::dec <<std::endl;
 
-  node_id = g_unpacker.get_fe_id("skseb"); // Event builder
-  std::cout << std::hex;
-  std::cout << g_unpacker.get_node_header(node_id, DAQNode::k_magic) << std::dec << std::endl;
-  std::cout << g_unpacker.get_node_header(node_id, DAQNode::k_data_size) << std::endl;
-  std::cout << g_unpacker.get_node_header(node_id, DAQNode::k_event_number) << std::endl;
-  std::cout << g_unpacker.get_node_header(node_id, DAQNode::k_run_number) << std::hex <<std::endl;
-  std::cout << g_unpacker.get_node_header(node_id, DAQNode::k_node_id) << std::endl;
-  std::cout << g_unpacker.get_node_header(node_id, DAQNode::k_node_type) << std::dec <<std::endl;
-  std::cout << g_unpacker.get_node_header(node_id, DAQNode::k_number_of_blocks) << std::dec <<std::endl;
-  std::cout << g_unpacker.get_node_header(node_id, DAQNode::k_unix_time) << std::dec <<std::endl;
+  for(int i = 0; i<24; ++i){
+    std::cout << g_unpacker.get_fe_info(node_id, 0x10000000, i, 0) << std::endl;
+  }
 
-  //  g_unpacker.dump_data_fe(node_id);
+  g_unpacker.dump_data_fe(node_id, 0x10000000, 3);
 
   return 0;
 }

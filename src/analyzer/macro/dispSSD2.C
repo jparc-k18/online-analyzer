7efac9b3b0dc9bb79cd3e6a93d07413d08b45f94
S.H. Hayakawa <shuhei@km.phys.sci.osaka-u.ac.jp>
2015-10-18 09:42:01 +0900

    modify dispSSD.C

--- src/analyzer/macro/dispSSD2.C
+++ src/analyzer/macro/dispSSD2.C
@@ -16,23 +16,23 @@ void dispSSD2()
   for(int l=0; l<n_layer; ++l){
     ssd2_id[0][l] = HistMaker::getUniqueID(kSSD2, 0, kADC2D,  l+1);
     ssd2_id[1][l] = HistMaker::getUniqueID(kSSD2, 0, kTDC2D,  l+1);
-    ssd2_id[2][l] = HistMaker::getUniqueID(kSSD2, 0, kHitPat, l+1);
-    ssd2_id[3][l] = HistMaker::getUniqueID(kSSD2, 0, kMulti,  l+1);
+    ssd2_id[2][l] = HistMaker::getUniqueID(kSSD2, 0, kHitPat, 2*(l+1));
+    ssd2_id[3][l] = HistMaker::getUniqueID(kSSD2, 0, kMulti,  2*(l+1));
   }
   // draw SSD
   for(int i=0; i<n_hist; ++i){
     TCanvas *c = (TCanvas*)gROOT->FindObject(Form("c%d", i+1));
+    if(!c) continue;
     c->Clear();
     c->Divide(2,2);
     for(int l=0; l<n_layer; ++l){
       c->cd(l+1)->SetGrid();
       TH1 *h = (TH1*)GHist::get(ssd2_id[i][l]);
+      if(!h) continue;
       h->Draw("colz");
     }
     c->Update();
   }
-
-  c->cd(0);
 
   // You must write these lines for the thread safe
   // ----------------------------------

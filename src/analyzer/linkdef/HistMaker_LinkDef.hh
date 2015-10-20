8cca9822855e43f41ddd19040abed9f87d84efc0
SKS <sks@sksterm5.user.k18net>
2015-10-21 04:26:45 +0900

    Remove comments from LinkDef.hh by honda

--- src/analyzer/linkdef/HistMaker_LinkDef.hh
+++ src/analyzer/linkdef/HistMaker_LinkDef.hh
@@ -2,7 +2,6 @@
 
 #ifdef __MAKECINT__
 
-//#pragma link C++ defined_in DetectorType;
 #pragma link C++ nestedclass;
 #pragma link C++ nestedtypedef;
 #pragma link C++ class std::vector<std::vector<int> >;

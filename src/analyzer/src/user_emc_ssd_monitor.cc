e2a5f4fa25a1f1c2015f1855f1cc5d46a5fb0973
S.H. Hayakawa <shuhei@km.phys.sci.osaka-u.ac.jp>
2017-06-09 00:17:35 +0900

    Modify user_emc_ssd_monitor.cc

--- src/analyzer/src/user_emc_ssd_monitor.cc
+++ src/analyzer/src/user_emc_ssd_monitor.cc
@@ -7,6 +7,8 @@
 #include <sstream>
 #include <vector>
 
+#include <TTimeStamp.h>
+
 #include "DetectorID.hh"
 #include "user_analyzer.hh"
 #include "UnpackerManager.hh"
@@ -17,28 +19,33 @@
 
 namespace analyzer
 {
-  using namespace hddaq::unpacker;
-  using namespace hddaq;
 
-  static const std::string space_name("analyzer");
+  namespace
+  {
+    using namespace hddaq::unpacker;
+    using namespace hddaq;
 
-  static const std::string& hostname = std::getenv("HOSTNAME");
-  static const std::string& exechost = "sksterm4";
-  static bool emc_alart[3] = { false, false, false };
-  static const double spill_prescale = 1.;
-  static const double emc_x_offset = 500000 - 303300;
-  static const double emc_y_offset = 500000 + 164000;
-  static const int NumOfAPVDAQ = 3;
-  static const int MaxEventBuffer = 100;
-  int  event_buffer = 0;
-  int  emc_state = -1;
-  std::bitset<NumOfAPVDAQ*(NumOfLayersSSD1+NumOfLayersSSD1)> ssd_bit;
-  bool ssdt_flag = false;
-  bool ssd1_flag[NumOfLayersSSD1][NumOfAPVDAQ][MaxEventBuffer];
-  bool ssd2_flag[NumOfLayersSSD2][NumOfAPVDAQ][MaxEventBuffer];
+    const std::string space_name("analyzer");
+
+    const std::string& hostname = std::getenv("HOSTNAME");
+    const std::string& exechost = "sksterm4";
+    bool emc_alart[3] = { false, false, false };
+    const Double_t spill_prescale = 1.;
+    const Double_t spill_cycle    = 5.52; // [s]
+    const Double_t emc_x_offset = 500000 - 303300;
+    const Double_t emc_y_offset = 500000 + 164000;
+    const Int_t NumOfAPVDAQ = 3;
+    const Int_t MaxEventBuffer = 100;
+    Int_t  event_buffer = 0;
+    Int_t  emc_state = -1;
+    std::bitset<NumOfAPVDAQ*(NumOfLayersSSD1+NumOfLayersSSD1)> ssd_bit;
+    bool ssdt_flag = false;
+    bool ssd1_flag[NumOfLayersSSD1][NumOfAPVDAQ][MaxEventBuffer];
+    bool ssd2_flag[NumOfLayersSSD2][NumOfAPVDAQ][MaxEventBuffer];
+  }
 
 //____________________________________________________________________________
-int
+Int_t
 process_begin( const std::vector<std::string>& argv )
 {
   static const std::string func_name("["+space_name+"::"+__func__+"()]");
@@ -48,11 +55,11 @@ process_begin( const std::vector<std::string>& argv )
   gConfMan.initializeEMCParamMan();
   if( !gConfMan.isGood() ) return -1;
 
-  for( int i=0; i<MaxEventBuffer; ++i ){
-    for( int apv=0; apv<NumOfAPVDAQ; ++apv ){
-      for( int l=0; l<NumOfLayersSSD1; ++l )
+  for( Int_t i=0; i<MaxEventBuffer; ++i ){
+    for( Int_t apv=0; apv<NumOfAPVDAQ; ++apv ){
+      for( Int_t l=0; l<NumOfLayersSSD1; ++l )
 	ssd1_flag[l][apv][i] = true;
-      for( int l=0; l<NumOfLayersSSD2; ++l )
+      for( Int_t l=0; l<NumOfLayersSSD2; ++l )
 	ssd2_flag[l][apv][i] = true;
     }
   }
@@ -61,14 +68,14 @@ process_begin( const std::vector<std::string>& argv )
 }
 
 //____________________________________________________________________________
-int
+Int_t
 process_end( void )
 {
   return 0;
 }
 
 //____________________________________________________________________________
-int
+Int_t
 process_event( void )
 {
   static const std::string func_name("["+space_name+"::"+__func__+"()]");
@@ -76,44 +83,44 @@ process_event( void )
   static UnpackerManager& g_unpacker  = GUnpacker::get_instance();
   static EMCParamMan&     emc_manager = EMCParamMan::GetInstance();
 
-  const int run_number   = g_unpacker.get_root()->get_run_number();
-  const int event_number = g_unpacker.get_event_number();
+  const Int_t run_number   = g_unpacker.get_root()->get_run_number();
+  const Int_t event_number = g_unpacker.get_event_number();
 
   event_buffer++;
   event_buffer = event_buffer%MaxEventBuffer;
 
-  for( int apv=0; apv<NumOfAPVDAQ; ++apv ){
-    for( int l=0; l<NumOfLayersSSD1; ++l )
+  for( Int_t apv=0; apv<NumOfAPVDAQ; ++apv ){
+    for( Int_t l=0; l<NumOfLayersSSD1; ++l )
       ssd1_flag[l][apv][event_buffer] = false;
-    for( int l=0; l<NumOfLayersSSD2; ++l )
+    for( Int_t l=0; l<NumOfLayersSSD2; ++l )
       ssd2_flag[l][apv][event_buffer] = false;
   }
 
   // EMC -----------------------------------------------------------
-  static const int nspill = emc_manager.NSpill()*spill_prescale;
-  static int spill  = 0;
-  static int rspill = 0;
+  static const Int_t nspill = emc_manager.NSpill()*spill_prescale;
+  static Int_t spill  = 0;
+  static Int_t rspill = 0;
   {
-    static const int k_device = g_unpacker.get_device_id("EMC");
-    static const int k_xpos   = g_unpacker.get_data_id("EMC", "xpos");
-    static const int k_ypos   = g_unpacker.get_data_id("EMC", "ypos");
-    static const int k_state  = g_unpacker.get_data_id("EMC", "state");
-    double xpos = -9999.;
-    double ypos = -9999.;
-    int xpos_nhit = g_unpacker.get_entries( k_device, 0, 0, 0, k_xpos );
+    static const Int_t k_device = g_unpacker.get_device_id("EMC");
+    static const Int_t k_xpos   = g_unpacker.get_data_id("EMC", "xpos");
+    static const Int_t k_ypos   = g_unpacker.get_data_id("EMC", "ypos");
+    static const Int_t k_state  = g_unpacker.get_data_id("EMC", "state");
+    Double_t xpos = -9999.;
+    Double_t ypos = -9999.;
+    Int_t xpos_nhit = g_unpacker.get_entries( k_device, 0, 0, 0, k_xpos );
     if( xpos_nhit!=0 ) xpos = g_unpacker.get( k_device, 0, 0, 0, k_xpos );
-    int ypos_nhit = g_unpacker.get_entries( k_device, 0, 0, 0, k_ypos );
+    Int_t ypos_nhit = g_unpacker.get_entries( k_device, 0, 0, 0, k_ypos );
     if( ypos_nhit!=0 ) ypos = g_unpacker.get( k_device, 0, 0, 0, k_ypos );
     xpos -= emc_x_offset;
     ypos -= emc_y_offset;
-    double pos2spill = emc_manager.Pos2Spill( xpos, ypos )*spill_prescale;
+    Double_t pos2spill = emc_manager.Pos2Spill( xpos, ypos )*spill_prescale;
     if( spill > pos2spill ){
       emc_alart[0] = false; emc_alart[1] = false; emc_alart[2] = false;
     }
     spill = pos2spill;
     rspill = nspill - spill;
 
-    int nhit = g_unpacker.get_entries( k_device, 0, 0, 0, k_state );
+    Int_t nhit = g_unpacker.get_entries( k_device, 0, 0, 0, k_state );
     if( nhit>0 )
       emc_state = g_unpacker.get( k_device, 0, 0, 0, k_state );
     else
@@ -122,30 +129,30 @@ process_event( void )
 
   // SSDT ---------------------------------------------------------
   {
-    static const int k_device = g_unpacker.get_device_id("SSDT");
-    static const int k_tdc    = g_unpacker.get_data_id("SSDT","tdc");
-    int nhit = g_unpacker.get_entries( k_device, 0, 0, 0, k_tdc );
+    static const Int_t k_device = g_unpacker.get_device_id("SSDT");
+    static const Int_t k_tdc    = g_unpacker.get_data_id("SSDT","tdc");
+    Int_t nhit = g_unpacker.get_entries( k_device, 0, 0, 0, k_tdc );
     if( nhit!=0 ){
-      int tdc  = g_unpacker.get( k_device, 0, 0, 0, k_tdc );
+      Int_t tdc  = g_unpacker.get( k_device, 0, 0, 0, k_tdc );
       if( tdc>0 ) ssdt_flag = true;
     }
   }
 
   // SSD1 ---------------------------------------------------------
   {
-    static const int k_device = g_unpacker.get_device_id("SSD1");
-    static const int k_adc    = g_unpacker.get_data_id("SSD1","adc");
-    static const int k_flag   = g_unpacker.get_data_id("SSD1","flag");
-    for( int l=0; l<NumOfLayersSSD1; ++l ){
-      for( int seg=0; seg<NumOfSegSSD1; ++seg ){
-	int  nhit_adc  = g_unpacker.get_entries( k_device, l, seg, 0, k_adc  );
-	int  nhit_flag = g_unpacker.get_entries( k_device, l, seg, 0, k_flag );
+    static const Int_t k_device = g_unpacker.get_device_id("SSD1");
+    static const Int_t k_adc    = g_unpacker.get_data_id("SSD1","adc");
+    static const Int_t k_flag   = g_unpacker.get_data_id("SSD1","flag");
+    for( Int_t l=0; l<NumOfLayersSSD1; ++l ){
+      for( Int_t seg=0; seg<NumOfSegSSD1; ++seg ){
+	Int_t  nhit_adc  = g_unpacker.get_entries( k_device, l, seg, 0, k_adc  );
+	Int_t  nhit_flag = g_unpacker.get_entries( k_device, l, seg, 0, k_flag );
 	if( nhit_flag==0 || nhit_adc==0 ) continue;
-	int flag = g_unpacker.get( k_device, l, seg, 0, k_flag );
+	Int_t flag = g_unpacker.get( k_device, l, seg, 0, k_flag );
 	if( flag==0 ) continue;
-	int adc = g_unpacker.get( k_device, l, seg, 0, k_adc );
+	Int_t adc = g_unpacker.get( k_device, l, seg, 0, k_adc );
 	if( adc>0 ){
-	  int apv = seg/(NumOfSegSSD1/NumOfAPVDAQ);
+	  Int_t apv = seg/(NumOfSegSSD1/NumOfAPVDAQ);
 	  ssd1_flag[l][apv][event_buffer] = true;
 	}
       }//for(seg)
@@ -154,19 +161,19 @@ process_event( void )
 
   // SSD2 ---------------------------------------------------------
   {
-    static const int k_device = g_unpacker.get_device_id("SSD2");
-    static const int k_adc    = g_unpacker.get_data_id("SSD2","adc");
-    static const int k_flag   = g_unpacker.get_data_id("SSD2","flag");
-    for( int l=0; l<NumOfLayersSSD2; ++l ){
-      for( int seg=0; seg<NumOfSegSSD2; ++seg ){
-	int  nhit_adc  = g_unpacker.get_entries( k_device, l, seg, 0, k_adc  );
-	int  nhit_flag = g_unpacker.get_entries( k_device, l, seg, 0, k_flag );
+    static const Int_t k_device = g_unpacker.get_device_id("SSD2");
+    static const Int_t k_adc    = g_unpacker.get_data_id("SSD2","adc");
+    static const Int_t k_flag   = g_unpacker.get_data_id("SSD2","flag");
+    for( Int_t l=0; l<NumOfLayersSSD2; ++l ){
+      for( Int_t seg=0; seg<NumOfSegSSD2; ++seg ){
+	Int_t  nhit_adc  = g_unpacker.get_entries( k_device, l, seg, 0, k_adc  );
+	Int_t  nhit_flag = g_unpacker.get_entries( k_device, l, seg, 0, k_flag );
 	if( nhit_flag==0 || nhit_adc==0 ) continue;
-	int flag = g_unpacker.get( k_device, l, seg, 0, k_flag );
+	Int_t flag = g_unpacker.get( k_device, l, seg, 0, k_flag );
 	if( flag==0 ) continue;
-	int adc = g_unpacker.get( k_device, l, seg, 0, k_adc );
+	Int_t adc = g_unpacker.get( k_device, l, seg, 0, k_adc );
 	if( adc>0 ){
-	  int apv = seg/(NumOfSegSSD2/NumOfAPVDAQ);
+	  Int_t apv = seg/(NumOfSegSSD2/NumOfAPVDAQ);
 	  ssd2_flag[l][apv][event_buffer] = true;
 	}
       }//for(seg)
@@ -174,14 +181,14 @@ process_event( void )
   }
 
   ssd_bit.reset();
-  for( int i=0; i<MaxEventBuffer; ++i ){
-    for( int apv=0; apv<NumOfAPVDAQ; ++apv ){
-      for( int l=0; l<NumOfLayersSSD1; ++l ){
+  for( Int_t i=0; i<MaxEventBuffer; ++i ){
+    for( Int_t apv=0; apv<NumOfAPVDAQ; ++apv ){
+      for( Int_t l=0; l<NumOfLayersSSD1; ++l ){
 	if( ssd1_flag[l][apv][i] ){
 	  ssd_bit.set(apv+l*NumOfAPVDAQ);
 	}
       }
-      for( int l=0; l<NumOfLayersSSD2; ++l ){
+      for( Int_t l=0; l<NumOfLayersSSD2; ++l ){
 	if( ssd2_flag[l][apv][i] ){
 	  ssd_bit.set(apv+l*NumOfAPVDAQ+NumOfAPVDAQ*NumOfLayersSSD1);
 	}
@@ -194,9 +201,9 @@ process_event( void )
 
   if( spill>=0 ){
     std::cout << "   Spill# " << std::setw(4) << spill << "/" << nspill;
-    int rsec  = int(rspill*5.52);
-    int rhour = rsec/3600;
-    int rmin  = rsec/60 - rhour*60;
+    Int_t rsec  = Int_t(rspill*spill_cycle);
+    Int_t rhour = rsec/3600;
+    Int_t rmin  = rsec/60 - rhour*60;
     rsec = rsec%60;
     std::cout << " -> ";
     if( rhour<1 ){
@@ -220,8 +227,12 @@ process_event( void )
 	system("sh /home/sks/bin/emc_spill_alart.sh");
       }
     }
+    TTimeStamp stamp;
+    stamp.Add( -TTimeStamp::GetZoneOffset() );
+    stamp.Add( Int_t(rspill*spill_cycle) );
     std::cout << std::setw(4) << rspill << " : "
-	      << rhour << "h " << rmin << "m " << rsec << "s\e[m" << std::endl;
+	      << rhour << "h " << rmin << "m " << rsec << "s\e[m" << std::endl
+	      << "   End Time  : " << stamp.AsString("s") << std::endl;
   }
 
   if( emc_state==0 ){

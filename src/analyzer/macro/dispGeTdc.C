b2194af5c8fcd769a1c14e14202f71bf80523d3d
S.H. Hayakawa <hayashu@post.j-parc.jp>
2020-12-08 02:34:10 +0900

    Debug dispGeTdc.C

--- src/analyzer/macro/dispGeTdc.C
+++ src/analyzer/macro/dispGeTdc.C
@@ -1,37 +1,32 @@
-// Updater belongs to the namespace hddaq::gui
+
 using namespace hddaq::gui;
 
+//_____________________________________________________________________________
 void dispGeTdc()
 {
-  // You must write these lines for the thread safe
-  // ----------------------------------
-  if(Updater::isUpdating()){return;}
-  Updater::setUpdating(true);
-  // ----------------------------------
+  if( Updater::isUpdating() )
+    return;
+  Updater::setUpdating( true );
 
-  const int NumOfSegGe = 16;
-  ////////// Ge TDC
-  {
-    const int n_type = 3;
-    const int tdc_type[n_type] = { kCRM, kTFA, kRST };
-    // draw TDC
-    for(int t=0; t<n_type; t++){
-      TCanvas *c = (TCanvas*)gROOT->FindObject(Form("c%d", t+1));
-      c->Clear();
-      c->Divide(4,4);
-      int base_id = HistMaker::getUniqueID(kGe, 0, tdc_type[t], 1);
-      for(int i=0; i<NumOfSegGe; i++){
-	c->cd(i+1);
-	gPad->SetLogy();
-	TH1 *h = (TH1*)GHist::get(base_id + i);
-	h->Draw();
-      }
-      c->Update();
+  std::vector<Int_t> base_id = {
+    HistMaker::getUniqueID(kGe, 0, kCRM),
+    HistMaker::getUniqueID(kGe, 0, kTFA),
+    HistMaker::getUniqueID(kGe, 0, kRST),
+  };
+
+  // draw TDC
+  for( Int_t t=0, n=base_id.size(); t<n; ++t ){
+    auto c = dynamic_cast<TCanvas*>( gROOT->FindObject( Form("c%d", t+1) ) );
+    c->Clear();
+    c->Divide( 4, 4 );
+    for( Int_t i=0; i<NumOfSegGe; ++i ){
+      c->cd( i+1 );
+      gPad->SetLogy();
+      auto h = GHist::get( base_id[t]+i );
+      h->Draw();
     }
+    c->Update();
   }
 
-  // You must write these lines for the thread safe
-  // ----------------------------------
-  Updater::setUpdating(false);
-  // ----------------------------------
+  Updater::setUpdating( false );
 }

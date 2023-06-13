9dadc1809662ae7ce65dfdc76048e8eb7898abf1
STakahashi <takahashi.shuji.47m@st.kyoto-u.ac.jp>
2023-06-13 14:44:30 +0900

    modify macro dispHitPat and add macro effALL

--- src/analyzer/macro/dispHitPat.C
+++ src/analyzer/macro/dispHitPat.C
@@ -15,6 +15,15 @@ void dispHitPat()
     c->cd();
   }
 
+  int n_layerbc3 = 6;
+  int n_layerbc4 = 6;
+  int n_layersdc1 = 6;
+  int n_layersdc2 = 4;
+  int n_layersdc3 = 4;
+  int n_layersdc4 = 4;
+  int n_layersdc5 = 4;
+
+
   // gROOT->SetStyle("Plain");
   // // TPDF *pdf = new TPDF("/home/sks/PSFile/pro/hitpat_e40.pdf", 112);
 
@@ -40,11 +49,10 @@ void dispHitPat()
   //   delete c;
   //   c = NULL;
   // }
-  {
-  // draw ADC
+{
   TCanvas *c = (TCanvas*)gROOT->FindObject("c1");
   c->Clear();
-  c->Divide(3,2);
+  c->Divide(6,3);
 
   TH1* h = NULL;
 
@@ -56,98 +64,186 @@ void dispHitPat()
   h->Draw();
 
   c->cd(2);
-  base_id = HistMaker::getUniqueID(kBFT, 0, kHitPat);
+  base_id = HistMaker::getUniqueID(kBFT, 0, kHitPat, 1);
   h = (TH1*)GHist::get(base_id);
   h->SetMinimum(0);
   h->Draw();
 
   c->cd(3);
-  base_id = HistMaker::getUniqueID(kBFT, 0, kHitPat,2);
+  base_id = HistMaker::getUniqueID(kBFT, 0, kHitPat, 2);
   h = (TH1*)GHist::get(base_id);
   h->SetMinimum(0);
   h->Draw();
 
   c->cd(4);
-  base_id = HistMaker::getUniqueID(kBC3, 0, kHitPat, 1);
-  h = (TH1*)GHist::get(base_id);
-  h->SetMinimum(0);
-  h->Draw();
-
-  c->cd(5);
-  base_id = HistMaker::getUniqueID(kBC4, 0, kHitPat, 5);
-  h = (TH1*)GHist::get(base_id);
-  h->SetMinimum(0);
-  h->Draw();
-
-  c->cd(6);
-  base_id = HistMaker::getUniqueID(kBH2, 0, kHitPat,1);
+  base_id = HistMaker::getUniqueID(kBH2, 0, kHitPat);
   h = (TH1*)GHist::get(base_id);
   h->SetMinimum(0);
   h->Draw();
   c->Update();
 
+  c->cd(5);
+  base_id = HistMaker::getUniqueID(kBAC, 0, kHitPat);
+  h = (TH1*)GHist::get(base_id);
+  h->SetMinimum(0);
+  h->Draw();
+  c->Update();
+
+  TH1* hh = NULL;
+
+  base_id = HistMaker::getUniqueID(kBC3, 0, kHitPat);
+  int base_id_ctot = HistMaker::getUniqueID(kBC3, 0, kHitPat, 1+kTOTcutOffset);
+  for(int i=0; i<n_layerbc3; i++){
+    c->cd(i+7);
+    h = (TH1*)GHist::get(base_id + i);
+    h->SetMinimum(0);
+    if( !h ) continue;
+    h->Draw();
+    hh = (TH1*)GHist::get(base_id_ctot + i);
+    hh->SetMinimum(0);
+    if( !hh ) continue;
+    hh->SetLineColor( kRed );
+    hh->Draw("same");
+  }
+
+
+  base_id = HistMaker::getUniqueID(kBC4, 0, kHitPat);
+  base_id_ctot = HistMaker::getUniqueID(kBC4, 0, kHitPat, 1+kTOTcutOffset);
+  for(int i=0; i<n_layerbc4; i++){
+    c->cd(i+7+n_layerbc3);
+    h = (TH1*)GHist::get(base_id + i);
+    h->SetMinimum(0);
+    if( !h ) continue;
+    h->Draw();
+    hh = (TH1*)GHist::get(base_id_ctot + i);
+    hh->SetMinimum(0);
+    if( !hh ) continue;
+    hh->SetLineColor( kRed );
+    hh->Draw("same");
+  }
+
+  c->Update();
+
   c->cd(0);
 }
 
-  // pdf->Close();
-
-
 {
-  // draw ADC
   TCanvas *c2 = (TCanvas*)gROOT->FindObject("c2");
   c2->Clear();
-  c2->Divide(4,2);
+  c2->Divide(8,4);
 
   TH1* h = NULL;
+  TH1* hh = NULL;
 
-  // Scattering
-  c2->cd(1);
-  int base_id = HistMaker::getUniqueID(kSDC1, 0, kHitPat, 1);
-  h = (TH1*)GHist::get(base_id);
-  h->SetMinimum(0);
-  h->Draw();
+  int base_id = HistMaker::getUniqueID(kSDC1, 0, kHitPat);
+  int base_id_ctot = HistMaker::getUniqueID(kSDC1, 0, kHitPat, 1+kTOTcutOffset);
+  for(int i=0; i<n_layersdc1; i++){
+    c2->cd(i+1);
+    h = (TH1*)GHist::get(base_id + i);
+    h->SetMinimum(0);
+    if( !h ) continue;
+    h->Draw();
+    hh = (TH1*)GHist::get(base_id_ctot + i);
+    hh->SetMinimum(0);
+    if( !hh ) continue;
+    hh->SetLineColor( kRed );
+    hh->Draw("same");
+  }
 
-  c2->cd(2);
-  base_id = HistMaker::getUniqueID(kSDC2, 0, kHitPat,1);
-  h = (TH1*)GHist::get(base_id);
-  h->SetMinimum(0);
-  h->Draw();
 
-  // c2->cd(3);
-  // base_id = HistMaker::getUniqueID(kSCH, 0, kHitPat);
-  // h = (TH1*)GHist::get(base_id);
-  // h->SetMinimum(0);
-  // h->Draw();
+  base_id = HistMaker::getUniqueID(kSDC2, 0, kHitPat);
+  base_id_ctot = HistMaker::getUniqueID(kSDC2, 0, kHitPat, 1+kTOTcutOffset);
+  for(int i=0; i<n_layersdc2; i++){
+    c2->cd(i+9);
+    h = (TH1*)GHist::get(base_id + i);
+    h->SetMinimum(0);
+    if( !h ) continue;
+    h->Draw();
+    hh = (TH1*)GHist::get(base_id_ctot + i);
+    hh->SetMinimum(0);
+    if( !hh ) continue;
+    hh->SetLineColor( kRed );
+    hh->Draw("same");
+  }
 
-  c2->cd(3);
-  base_id = HistMaker::getUniqueID(kSDC3, 0, kHitPat, 1);
-  h = (TH1*)GHist::get(base_id);
-  h->SetMinimum(0);
-  h->Draw();
+  base_id = HistMaker::getUniqueID(kSDC3, 0, kHitPat);
+  base_id_ctot = HistMaker::getUniqueID(kSDC3, 0, kHitPat, 11);
+  for(int i=0; i<n_layersdc3; i++){
+    c2->cd(i+9+n_layersdc2);
+    h = (TH1*)GHist::get(base_id + i);
+    h->SetMinimum(0);
+    if( !h ) continue;
+    h->Draw();
+    hh = (TH1*)GHist::get(base_id_ctot + i);
+    hh->SetMinimum(0);
+    if( !hh ) continue;
+    hh->SetLineColor( kRed );
+    hh->Draw("same");
+  }
 
-  c2->cd(4);
-  base_id = HistMaker::getUniqueID(kSDC4, 0, kHitPat, 3);
-  h = (TH1*)GHist::get(base_id);
-  h->SetMinimum(0);
-  h->Draw();
+  base_id = HistMaker::getUniqueID(kSDC4, 0, kHitPat);
+  base_id_ctot = HistMaker::getUniqueID(kSDC4, 0, kHitPat, 11);
+  for(int i=0; i<n_layersdc4; i++){
+    c2->cd(i+9+n_layersdc2+n_layersdc3);
+    h = (TH1*)GHist::get(base_id + i);
+    h->SetMinimum(0);
+    if( !h ) continue;
+    h->Draw();
+    hh = (TH1*)GHist::get(base_id_ctot + i);
+    hh->SetMinimum(0);
+    if( !hh ) continue;
+    hh->SetLineColor( kRed );
+    hh->Draw("same");
+  }
 
-  c2->cd(5);
+
+  base_id = HistMaker::getUniqueID(kSDC5, 0, kHitPat);
+  base_id_ctot = HistMaker::getUniqueID(kSDC5, 0, kHitPat, 11);
+  for(int i=0; i<n_layersdc5; i++){
+    c2->cd(i+9+n_layersdc2+n_layersdc3+n_layersdc4);
+    h = (TH1*)GHist::get(base_id + i);
+    h->SetMinimum(0);
+    if( !h ) continue;
+    h->Draw();
+    hh = (TH1*)GHist::get(base_id_ctot + i);
+    hh->SetMinimum(0);
+    if( !hh ) continue;
+    hh->SetLineColor( kRed );
+    hh->Draw("same");
+  }
+
+  c2->cd(25);
   base_id = HistMaker::getUniqueID(kTOF, 0, kHitPat);
   h = (TH1*)GHist::get(base_id);
   h->SetMinimum(0);
   h->Draw();
 
-  c2->cd(6);
+  c2->cd(26);
   base_id = HistMaker::getUniqueID(kAC1, 0, kHitPat);
   h = (TH1*)GHist::get(base_id);
   h->SetMinimum(0);
   h->Draw();
 
-  c2->cd(7);
+  c2->cd(27);
   base_id = HistMaker::getUniqueID(kWC, 0, kHitPat);
   h = (TH1*)GHist::get(base_id);
   h->SetMinimum(0);
   h->Draw();
+
+  c2->cd(28);
+  base_id = HistMaker::getUniqueID(kSAC3, 0, kTDC);
+  h = (TH1*)GHist::get(base_id);
+  h->SetMinimum(0);
+  h->Draw();
+  c2->cd(29);
+  h = (TH1*)GHist::get(base_id + 1);
+  h->SetMinimum(0);
+  h->Draw();
+
+  c2->Update();
+
+
+  c2->cd(0);
 
 }
 
---
+++ src/analyzer/macro/effALL.C
@@ -0,0 +1,290 @@
+// Updater belongs to the namespace hddaq::gui
+using namespace hddaq::gui;
+
+void effALL( void )
+{
+  // You must write these lines for the thread safe
+  // ----------------------------------
+  if(Updater::isUpdating()){return;}
+  Updater::setUpdating(true);
+  // ----------------------------------
+
+  {
+    Int_t n_layer = 6;
+    TCanvas *c = (TCanvas*)gROOT->FindObject("c1");
+    c->Clear();
+    c->Divide(6,3);
+    Int_t base_id = HistMaker::getUniqueID(kBH1, 0, kMulti);
+    c->cd(1);
+    {
+      TH1 *h_wt = GHist::get(base_id);
+      h_wt->Draw();
+      double Nof0     = h_wt->GetBinContent(1);
+      double NofTotal = h_wt->GetEntries();
+      double eff      = 1. - (double)Nof0/NofTotal;
+
+      double xpos  = h_wt->GetXaxis()->GetBinCenter(h_wt->GetNbinsX())*0.3;
+      double ypos  = h_wt->GetMaximum()*0.8;
+      TLatex *text = new TLatex(xpos, ypos, Form("eff. %.2f", eff));
+      text->SetTextSize(0.08);
+      text->Draw();
+    }
+
+    base_id = HistMaker::getUniqueID(kBFT, 0, kMulti);
+    c->cd(2);
+    {
+      TH1 *h_wt = GHist::get(base_id);
+      h_wt->Draw();
+      double Nof0     = h_wt->GetBinContent(1);
+      double NofTotal = h_wt->GetEntries();
+      double eff      = 1. - (double)Nof0/NofTotal;
+
+      double xpos  = h_wt->GetXaxis()->GetBinCenter(h_wt->GetNbinsX())*0.3;
+      double ypos  = h_wt->GetMaximum()*0.8;
+      TLatex *text = new TLatex(xpos, ypos, Form("eff. %.2f", eff));
+      text->SetTextSize(0.08);
+      text->Draw();
+    }
+
+
+    base_id = HistMaker::getUniqueID(kBH2, 0, kMulti);
+    c->cd(3);
+    {
+      TH1 *h_wt = GHist::get(base_id);
+      h_wt->Draw();
+      double Nof0     = h_wt->GetBinContent(1);
+      double NofTotal = h_wt->GetEntries();
+      double eff      = 1. - (double)Nof0/NofTotal;
+
+      double xpos  = h_wt->GetXaxis()->GetBinCenter(h_wt->GetNbinsX())*0.3;
+      double ypos  = h_wt->GetMaximum()*0.8;
+      TLatex *text = new TLatex(xpos, ypos, Form("eff. %.2f", eff));
+      text->SetTextSize(0.08);
+      text->Draw();
+    }
+
+    base_id = HistMaker::getUniqueID(kBAC, 0, kMulti);
+    c->cd(4);
+    {
+      TH1 *h_wt = GHist::get(base_id);
+      h_wt->Draw();
+      double Nof0     = h_wt->GetBinContent(1);
+      double NofTotal = h_wt->GetEntries();
+      double eff      = 1. - (double)Nof0/NofTotal;
+
+      double xpos  = h_wt->GetXaxis()->GetBinCenter(h_wt->GetNbinsX())*0.3;
+      double ypos  = h_wt->GetMaximum()*0.8;
+      TLatex *text = new TLatex(xpos, ypos, Form("eff. %.2f", eff));
+      text->SetTextSize(0.08);
+      text->Draw();
+    }
+
+    base_id = HistMaker::getUniqueID(kBC3, 0, kMulti);
+    for( Int_t i=0; i<n_layer; ++i ){
+      c->cd(i+7);
+      TH1 *h_wt = GHist::get(base_id +i +n_layer);
+      h_wt->Draw();
+
+      double Nof0     = h_wt->GetBinContent(1);
+      double NofTotal = h_wt->GetEntries();
+      double eff      = 1. - (double)Nof0/NofTotal;
+
+      double xpos  = h_wt->GetXaxis()->GetBinCenter(h_wt->GetNbinsX())*0.3;
+      double ypos  = h_wt->GetMaximum()*0.8;
+      TLatex *text = new TLatex(xpos, ypos, Form("plane eff. %.4f", eff));
+      text->SetTextSize(0.08);
+      text->Draw();
+    }
+    c->Modified();
+    c->Update();
+
+  // draw Multi with plane efficiency BC4
+
+    base_id = HistMaker::getUniqueID(kBC4, 0, kMulti);
+    for( Int_t i=0; i<n_layer; ++i ){
+      c->cd(i+13);
+      TH1 *h_wt = GHist::get(base_id +i +n_layer);
+      h_wt->Draw();
+
+      double Nof0     = h_wt->GetBinContent(1);
+      double NofTotal = h_wt->GetEntries();
+      double eff      = 1. - (double)Nof0/NofTotal;
+
+      double xpos  = h_wt->GetXaxis()->GetBinCenter(h_wt->GetNbinsX())*0.3;
+      double ypos  = h_wt->GetMaximum()*0.8;
+      TLatex *text = new TLatex(xpos, ypos, Form("plane eff. %.4f", eff));
+      text->SetTextSize(0.08);
+      text->Draw();
+    }
+
+    c->Modified();
+    c->Update();
+  }
+
+  // draw Multi with plane efficiency SDC1
+  {
+    Int_t n_layer = 6;
+    TCanvas *c = (TCanvas*)gROOT->FindObject("c2");
+    c->Clear();
+    c->Divide(6,5);
+    Int_t base_id = HistMaker::getUniqueID(kSDC1, 0, kMulti);
+    for( Int_t i=0; i<n_layer; ++i ){
+      c->cd(i+1);
+      TH1 *h_wt = GHist::get(base_id +i +n_layer);
+      h_wt->Draw();
+
+      double Nof0     = h_wt->GetBinContent(1);
+      double NofTotal = h_wt->GetEntries();
+      double eff      = 1. - (double)Nof0/NofTotal;
+
+      double xpos  = h_wt->GetXaxis()->GetBinCenter(h_wt->GetNbinsX())*0.3;
+      double ypos  = h_wt->GetMaximum()*0.8;
+      TLatex *text = new TLatex(xpos, ypos, Form("plane eff. %.4f", eff));
+      text->SetTextSize(0.08);
+      text->Draw();
+    }
+    c->Modified();
+    c->Update();
+
+  // draw Multi with plane efficiency SDC2
+    n_layer = 4;
+    base_id = HistMaker::getUniqueID(kSDC2, 0, kMulti);
+    for( Int_t i=0; i<n_layer; ++i ){
+      c->cd(i+7);
+      TH1 *h = GHist::get(base_id +i);
+      h->Draw();
+      TH1 *h_wt = GHist::get(base_id +i +n_layer);
+      if( !h_wt ) continue;
+      h_wt->SetLineColor(kBlue);
+      h_wt->Draw("same");
+      double Nof0     = h->GetBinContent(1);
+      double NofTotal = h->GetEntries();
+      double eff      = 1. - (double)Nof0/NofTotal;
+      double Nof0_wt     = h_wt->GetBinContent(1);
+      double NofTotal_wt = h_wt->GetEntries();
+      double eff_wt      = 1. - (double)Nof0_wt/NofTotal_wt;
+
+      double xpos  = h_wt->GetXaxis()->GetBinCenter(h_wt->GetNbinsX())*0.3;
+      double ypos  = h_wt->GetMaximum()*0.5;
+      TLatex *text = new TLatex(xpos, ypos, Form("plane eff. %.4f(%.4f)", eff, eff_wt));
+      text->SetTextSize(0.08);
+      text->Draw();
+    }
+
+    c->Modified();
+    c->Update();
+  // draw Multi with plane efficiency SDC3
+
+    base_id = HistMaker::getUniqueID(kSDC3, 0, kMulti);
+    for( Int_t i=0; i<n_layer; ++i ){
+      c->cd(i+11);
+      TH1 *h_wt = GHist::get(base_id +i +n_layer);
+      h_wt->Draw();
+
+      double Nof0     = h_wt->GetBinContent(1);
+      double NofTotal = h_wt->GetEntries();
+      double eff      = 1. - (double)Nof0/NofTotal;
+
+      double xpos  = h_wt->GetXaxis()->GetBinCenter(h_wt->GetNbinsX())*0.3;
+      double ypos  = h_wt->GetMaximum()*0.8;
+      TLatex *text = new TLatex(xpos, ypos, Form("plane eff. %.2f", eff));
+      text->SetTextSize(0.08);
+      text->Draw();
+    }
+
+    c->Modified();
+    c->Update();
+
+    // draw Multi with plane efficiency SDC4
+    base_id = HistMaker::getUniqueID(kSDC4, 0, kMulti);
+    for( Int_t i=0; i<n_layer; ++i ){
+      c->cd(i+15);
+      TH1 *h_wt = GHist::get(base_id +i +n_layer);
+      h_wt->Draw();
+
+      double Nof0     = h_wt->GetBinContent(1);
+      double NofTotal = h_wt->GetEntries();
+      double eff      = 1. - (double)Nof0/NofTotal;
+
+      double xpos  = h_wt->GetXaxis()->GetBinCenter(h_wt->GetNbinsX())*0.3;
+      double ypos  = h_wt->GetMaximum()*0.8;
+      TLatex *text = new TLatex(xpos, ypos, Form("plane eff. %.2f", eff));
+      text->SetTextSize(0.08);
+      text->Draw();
+    }
+    c->Modified();
+    c->Update();
+
+    // draw Multi with plane efficiency SDC5
+    base_id = HistMaker::getUniqueID(kSDC5, 0, kMulti);
+    for( Int_t i=0; i<n_layer; ++i ){
+      c->cd(i+19);
+      TH1 *h_wt = GHist::get(base_id +i +n_layer);
+      h_wt->Draw();
+
+      double Nof0     = h_wt->GetBinContent(1);
+      double NofTotal = h_wt->GetEntries();
+      double eff      = 1. - (double)Nof0/NofTotal;
+
+      double xpos  = h_wt->GetXaxis()->GetBinCenter(h_wt->GetNbinsX())*0.3;
+      double ypos  = h_wt->GetMaximum()*0.8;
+      TLatex *text = new TLatex(xpos, ypos, Form("plane eff. %.2f", eff));
+      text->SetTextSize(0.08);
+      text->Draw();
+    }
+    c->Modified();
+    c->Update();
+
+    base_id = HistMaker::getUniqueID(kTOF, 0, kMulti);
+    c->cd(25);
+    {
+      TH1 *h_wt = GHist::get(base_id);
+      h_wt->Draw();
+      double Nof0     = h_wt->GetBinContent(1);
+      double NofTotal = h_wt->GetEntries();
+      double eff      = 1. - (double)Nof0/NofTotal;
+
+      double xpos  = h_wt->GetXaxis()->GetBinCenter(h_wt->GetNbinsX())*0.3;
+      double ypos  = h_wt->GetMaximum()*0.8;
+      TLatex *text = new TLatex(xpos, ypos, Form("eff. %.2f", eff));
+      text->SetTextSize(0.08);
+      text->Draw();
+    }
+
+    base_id = HistMaker::getUniqueID(kAC1, 0, kMulti);
+    c->cd(26);
+    {
+      TH1 *h_wt = GHist::get(base_id);
+      h_wt->Draw();
+      double Nof0     = h_wt->GetBinContent(1);
+      double NofTotal = h_wt->GetEntries();
+      double eff      = 1. - (double)Nof0/NofTotal;
+
+      double xpos  = h_wt->GetXaxis()->GetBinCenter(h_wt->GetNbinsX())*0.3;
+      double ypos  = h_wt->GetMaximum()*0.8;
+      TLatex *text = new TLatex(xpos, ypos, Form("eff. %.2f", eff));
+      text->SetTextSize(0.08);
+      text->Draw();
+    }
+    base_id = HistMaker::getUniqueID(kWC, 0, kMulti);
+    c->cd(27);
+    {
+      TH1 *h_wt = GHist::get(base_id);
+      h_wt->Draw();
+      double Nof0     = h_wt->GetBinContent(1);
+      double NofTotal = h_wt->GetEntries();
+      double eff      = 1. - (double)Nof0/NofTotal;
+
+      double xpos  = h_wt->GetXaxis()->GetBinCenter(h_wt->GetNbinsX())*0.3;
+      double ypos  = h_wt->GetMaximum()*0.8;
+      TLatex *text = new TLatex(xpos, ypos, Form("eff. %.2f", eff));
+      text->SetTextSize(0.08);
+      text->Draw();
+    }
+  }
+
+  // You must write these lines for the thread safe
+  // ----------------------------------
+  Updater::setUpdating(false);
+  // ----------------------------------
+}

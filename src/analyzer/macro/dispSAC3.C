737ae5de38db30532787744e05f58c5d281bad2b
Daigo Watanabe <daigo@lambda.phys.tohoku.ac.jp>
2023-06-17 08:37:25 +0900

    Modify dispSAC3

--- src/analyzer/macro/dispSAC3.C
+++ src/analyzer/macro/dispSAC3.C
@@ -1,6 +1,8 @@
 // Updater belongs to the namespace hddaq::gui
 using namespace hddaq::gui;
 
+#include "UserParamMan.hh"
+
 void dispSAC3()
 {
   // You must write these lines for the thread safe
@@ -17,9 +19,8 @@ void dispSAC3()
   {
     TCanvas *c = (TCanvas*)gROOT->FindObject("c1");
     c->Clear();
-    c->Divide(2,1);
+    c->Divide(1,1);
     int base_id = HistMaker::getUniqueID(kSAC3, 0, kADC);
-    //int adcwtdc_id = HistMaker::getUniqueID(kSAC3, 0 , kADCwTDC);
     for(int i = 0; i<1; ++i){
       c->cd(i+1);
       gPad->SetLogy();
@@ -27,50 +28,44 @@ void dispSAC3()
       if( !h ) continue;
       h->GetXaxis()->SetRangeUser( 0 , 4096 );
       h->Draw();
-     // TH1 *hh =(TH1*)GHist::get(adcwtdc_id + i);
-     // if( !hh ) continue;
-     // hh->GetXaxis()->SetRangeUser( 0 , 4096 );
-     // hh->SetLineColor( kRed );
-     // hh->Draw("same");
 
       }
     c->Update();
   }
-  // draw ADC and ADCwTDC
+
+  // draw ADCwTDC
   {
     TCanvas *c = (TCanvas*)gROOT->FindObject("c2");
     c->Clear();
     c->Divide(2,1);
     int base_id = HistMaker::getUniqueID(kSAC3, 0, kADC);
     int adcwtdc_id = HistMaker::getUniqueID(kSAC3, 0 , kADCwTDC);
-//    for(int i = 0; i<1; ++i){
-//      c->cd(i+1);
-//      gPad->SetLogy();
-//      TH1 *h1 = (TH1*)GHist::get(base_id + i);
-//      if( !h ) continue;
-//      h->GetXaxis()->SetRangeUser( 0 , 4096 );
-//      h->Draw();
-//      TH1 *hh =(TH1*)GHist::get(adcwtdc_id + i);
-//      if( !hh ) continue;
-//      hh->GetXaxis()->SetRangeUser( 0 , 4096 );
-//      hh->SetLineColor( kRed );
-//      hh->Draw("same");
-      c->cd(i+1);
+
+      c->cd(0+1);
       gPad->SetLogy();
-      TH1 *h1 = (TH1*)GHist::get(base_id + i);
-      if( !h ) continue;
+      TH1 *h = (TH1*)GHist::get(base_id + 0);
       h->GetXaxis()->SetRangeUser( 0 , 4096 );
       h->Draw();
-      TH1 *hh =(TH1*)GHist::get(adcwtdc_id + i);
-      if( !hh ) continue;
+      TH1 *hh = (TH1*)GHist::get(adcwtdc_id + 0);
       hh->GetXaxis()->SetRangeUser( 0 , 4096 );
       hh->SetLineColor( kRed );
+      //hh->SetTitle("SAC3_ADCwTDC_1");
       hh->Draw("same");
 
+      c->cd(1+1);
+      gPad->SetLogy();
+      h = GHist::get(base_id + 0);
+      h->GetXaxis()->SetRangeUser( 0 , 4096 );
+      h->Draw();
+      hh = GHist::get(adcwtdc_id + 1);
+      hh->GetXaxis()->SetRangeUser( 0 , 4096 );
+      hh->SetLineColor( kRed );
+      //hh->SetTitle("SAC3_ADCwTDC_2");
+      hh->Draw("same");
 
-//      }
     c->Update();
   }
+
   // draw TDC
   {
     TCanvas *c = (TCanvas*)gROOT->FindObject("c3");

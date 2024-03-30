2d9aedd827d5c29741a87e687d69ecd76008ed35
kevata <ebata.kengo.84r@st.kyoto-u.ac.jp>
2024-03-30 22:53:03 +0900

    dispBAC modified (add BAC2)

--- src/analyzer/macro/dispBAC.C
+++ src/analyzer/macro/dispBAC.C
@@ -15,24 +15,26 @@ dispBAC( void )
   {
     auto c1 = dynamic_cast<TCanvas*>(gROOT->FindObject("c1"));
     c1->Clear();
-    c1->Divide(2, 1);
+    c1->Divide(2, 2);
     auto adc_hid = HistMaker::getUniqueID(kBAC, 0, kADC);
     auto awt_hid = HistMaker::getUniqueID(kBAC, 0, kADCwTDC);
     auto tdc_hid = HistMaker::getUniqueID(kBAC, 0, kTDC);
-    // ADC
-    c1->cd(1)->SetLogy();
-    auto h1 = dynamic_cast<TH1*>(GHist::get(adc_hid));
-    // auto h1->GetXaxis()->SetRangeUser(0, 0x1000);
-    h1->Draw();
-    // ADCwTDC
-    auto h2 = dynamic_cast<TH1*>(GHist::get(awt_hid));
-    h2->SetLineColor(kRed+1);
-    h2->Draw("same");
-    // TDC
-    c1->cd(2);//->SetLogy();
-    auto h3 = dynamic_cast<TH1*>(GHist::get(tdc_hid));
-    // h3->GetXaxis()->SetRangeUser(0, 2000000);
-    h3->Draw();
+    for (int i =0; i<2;++i){
+      // ADC
+      c1->cd(2*i+1)->SetLogy();
+      auto h1 = dynamic_cast<TH1*>(GHist::get(adc_hid+i));
+      // auto h1->GetXaxis()->SetRangeUser(0, 0x1000);
+      h1->Draw();
+      // ADCwTDC
+      auto h2 = dynamic_cast<TH1*>(GHist::get(awt_hid+i));
+      h2->SetLineColor(kRed+1);
+      h2->Draw("same");
+      // TDC
+      c1->cd(2*i+2);//->SetLogy();
+      auto h3 = dynamic_cast<TH1*>(GHist::get(tdc_hid+i));
+      // h3->GetXaxis()->SetRangeUser(0, 2000000);
+      h3->Draw();
+    }
     c1->Update();
   }
 

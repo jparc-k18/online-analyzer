ab8e92fcad226aea9584a801927db78465b6e414
Chesu Seong <seong.che.su.s7@dc.tohoku.ac.jp>
2025-11-02 22:11:36 +0900

    Modify dispBGO.C

--- src/analyzer/macro/dispBGO.C
+++ src/analyzer/macro/dispBGO.C
@@ -9,29 +9,47 @@ void dispBGO()
   Updater::setUpdating(true);
   // ----------------------------------
 
-  const int NumOfSegBGO = 48;
-  const int NumOfSlot = 4;
+  int NumOfSegClBGO = 60;
+  int NumOfSlotCl = 5;
+  int NumOfSegSingleBGO = 18;
+  int NumOfSlotSingle   = 3;
 
   ////////// BGO TDC
   {
-    for(int slot=0; slot<NumOfSlot; slot++){
+    // Clover
+    for(int slot=0; slot<NumOfSlotCl; slot++){
       TCanvas *c = (TCanvas*)gROOT->FindObject(Form("c%d", slot+1));
       c->Clear();
       c->Divide(3,4);
       int base_id = HistMaker::getUniqueID(kBGO, 0, kTDC, 1);
-      for(int i=0; i<NumOfSegBGO/NumOfSlot; i++){
+      for(int i=0; i<NumOfSegClBGO/NumOfSlotCl; i++){
 	c->cd(i+1);
-	TH1 *h = (TH1*)GHist::get(base_id + i + slot*NumOfSegBGO/NumOfSlot);
+	TH1 *h = (TH1*)GHist::get(base_id + i + slot*NumOfSegClBGO/NumOfSlotCl);
 	h->Draw();
       }
       c->Update();
     }
+
+    // Single
+    for(int slot=0; slot<NumOfSlotSingle; slot++){
+      TCanvas *c = (TCanvas*)gROOT->FindObject(Form("c%d", slot+6));
+      c->Clear();
+      c->Divide(3,2);
+      int base_id = HistMaker::getUniqueID(kBGO, 0, kTDC, 1);
+      for(int i=0; i<NumOfSegSingleBGO/NumOfSlotSingle; i++){
+	c->cd(i+1);
+	TH1 *h = (TH1*)GHist::get(base_id + i + 60 + slot*NumOfSegSingleBGO/NumOfSlotSingle);
+	h->Draw();
+      }
+      c->Update();
+    }
+
   }
 
   ////////// BGO TDC 2D
   {
 
-      TCanvas *c = (TCanvas*)gROOT->FindObject(Form("c%d", 5));
+      TCanvas *c = (TCanvas*)gROOT->FindObject(Form("c%d", 9));
       c->Clear();
       int base_id = HistMaker::getUniqueID(kBGO, 0, kTDC2D, 1);
       TH2 *h2 = (TH2*)GHist::get(base_id);
